9a11d0c7f54f4f4e771d189e3c1d5ab9efcc0166
dongocthanhchau <dongocthanhchau@gmail.com>
2018-11-09 16:17:37 +0700

    Update led7_595.h

--- led7_595.h
+++ led7_595.h
@@ -3,17 +3,17 @@
 
 #include "stdint.h"
 
-#define DATACLOCK_PIN 0
-#define STORAGECLOCK_PIN 4
-#define DATA_PIN 5
+#define DATACLOCK_PIN 0 //DS PIN IN 74HC595 (PIN14)
+#define STORAGECLOCK_PIN 4 //ST_CP PIN IN 74HC595 (PIN12)
+#define DATA_PIN 5 // SH_CP PIN IN 54HC595 (PIN11)
 
-#define SCAN 1
+#define SCAN 1 //delay time after display 1 digic
 
 //extern uint8_t gLED[10];
 //extern uint8_t gDigit[4];
 
 extern void dataSend(uint8_t vData);
 extern void dataLatch(uint8_t vData);
-extern void ledDisplay(uint8_t vHour, uint8_t vMin);
-
+extern void ledDisplay(uint8_t vHour, uint8_t vMin);//use this in loop funtion to export data to LED7Seg
+//vHour,vMin: time to display
 #endif
