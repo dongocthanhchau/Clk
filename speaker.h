8817f12cbb331eb17b615211def4eea2f1c7c82a
dongocthanhchau <dongocthanhchau@gmail.com>
2018-11-09 16:14:07 +0700

    Update speaker.h

--- speaker.h
+++ speaker.h
@@ -5,7 +5,11 @@
 
 #define TIMER 70
 #define PERIOD 400
-void setAlarm(uint8_t data);
-void outputAlarm();
+
+void setAlarm(uint8_t data);//use to set alarm
+// data=1: set Timer Alarm (turn off until setAlarm(0))
+// data=2: set period Alarm (turn off after 2 tone)
+// data=0: turn off all sound
+void outputAlarm(); //use this in loop function
 
 #endif
