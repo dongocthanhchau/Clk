28a07422fb12c01f5eacf236d9cbf7fa4dde9170
dongocthanhchau <dongocthanhchau@gmail.com>
2018-11-09 16:48:40 +0700

    Update speaker.h

--- speaker.h
+++ speaker.h
@@ -11,5 +11,7 @@ void setAlarm(uint8_t data);//use to set alarm
 // data=2: set period Alarm (turn off after 2 tone)
 // data=0: turn off all sound
 void outputAlarm(); //use this in loop function
+void checkAlarm(uint8_t vHour, uint8_t vMin,uint8_t gHour, uint8_t gMin); // Check and set alarm
+void setPeriod(uint8_t vMin); //use this in loop to check period time in minute
 
 #endif
