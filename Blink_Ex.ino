e8fb5875af81aa32274877d92c5b54106e46370e
dongocthanhchau <dongocthanhchau@gmail.com>
2018-11-09 16:09:30 +0700

    Update Blink_Ex.ino

--- Blink_Ex.ino
+++ Blink_Ex.ino
@@ -1,12 +1,10 @@
 #include "led7_595.h"
 #include "speaker.h"
 
-int alarm[2]={1,40};
+int alarm[2]={1,40}; // Test Code 
 
-//uint8_t gLED[11]={0x05,0x7D,0x46,0x54,0x3C,0x94,0x84,0x5D,0x04,0x14,0xFF};
 
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
   Serial.begin(9600);
   pinMode(SPEAKER_PIN, OUTPUT);
   pinMode(DATACLOCK_PIN, OUTPUT);
@@ -18,7 +16,7 @@ void setup() {
 
 unsigned long gTime=0;
 int i=0;
-// the loop function runs over and over again forever
+//Create a example 
 void loop() {
   if ( (millis()-gTime)>=1000 ) {i++;gTime=millis();}
   //dataSend(8);
